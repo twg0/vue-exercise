1242f20fd28ea336d115f9721809960012029bf9
twg0 <fruitsjh@gmail.com>
2023-10-04 00:06:55 +0900

    Vue 시작하기

---
+++ vue3-test/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+    <h1 @click="increase">{{ count }}</h1>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-if="count > 4">
+        4보다 큽니다!
+    </div>
+    <ul>
+        <FruitNames v-for="fruit in fruits" :key="fruit" :name="fruit">
+            {{ fruit }}
+        </FruitNames>
+    </ul>
+</template>
+
+<script>
+import FruitNames from './components/FruitNames.vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+    name: 'App',
+    components: {
+        HelloWorld,
+        FruitNames
+    },
+    data() {
+        return {
+            count: 0,
+            fruits: ['Apple', 'Banana', 'Cherry']
+        }
+    },
+    methods: {
+        increase() {
+            this.count += 1
+        }
+    }
+}
+</script>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+
+h1 {
+    font-size: 50px;
+    color: royalblue;
+}
+
+li {
+    list-style: none;
+}
+</style>
---
+++ vue3-test/src/components/FruitNames.vue
@@ -0,0 +1,20 @@
+<template>
+    <li>{{ name }}?!</li>
+</template>
+
+<script>
+export default {
+    props: {
+        name: {
+            type: String,
+            default: ''
+        }
+    }
+}
+</script>
+
+<style scoped>
+h1 {
+    color: red !important;
+}
+</style>
