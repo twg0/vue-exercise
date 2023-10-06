815bfb30e73125d5a7b6ae6d9cfa890767ca780c
twg0 <fruitsjh@gmail.com>
2023-10-06 15:35:39 +0900

    Computed 캐싱

--- vue3-computed/src/App.vue
+++ vue3-computed/src/App.vue
@@ -1,16 +1,33 @@
 <template>
-    <FruitNames/>
+    <h1>{{ reversedMessage }}</h1>
+    <h1>{{ reversedMessage }}</h1>
+    <h1>{{ reversedMessage }}</h1>
+    <h1>{{ reversedMessage }}</h1>
 </template>
 
 <script>
-import FruitNames from './components/FruitNames'
 export default {
-    components: {
-        FruitNames
-    }
+    data() {
+        return {
+            msg: 'Hello Computed!'
+        }
+    },
+    computed: {
+        // computed 에서 이미 계산된 요소는 재사용될 때 캐싱되어 사용됨
+        reversedMessage() {
+            return this.msg.split('').reverse().join('')
+        }
+    },
+    // methods: {
+    //     reverseMessage() {
+    //         return this.msg.split('').reverse().join('')
+    //     }
+    // }
 }
 </script>
 
 <style>
-
+h1,h2,h2,h3,h4,h5,h6 {
+    margin: 0;
+}
 </style>
