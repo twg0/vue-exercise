9ae8c5f34685566cb38daa7de500af159b1a494f
twg0 <fruitsjh@gmail.com>
2023-10-06 15:15:41 +0900

    Computed

---
+++ vue3-computed/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <FruitNames/>
+</template>
+
+<script>
+import FruitNames from './components/FruitNames'
+export default {
+    components: {
+        FruitNames
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue3-computed/src/components/FruitNames.vue
@@ -0,0 +1,52 @@
+<template>
+    <section v-if="hasFruit">
+        <h1>Fruits</h1>
+        <ul>
+            <li v-for="fruit in fruits" :key="fruit">
+                {{ fruit }}
+            </li>
+        </ul>
+    </section>
+    <section>
+        <h1>Reverse Fruits</h1>
+        <ul>
+            <li v-for="fruit in reverseFruits" :key="fruit">
+                {{ fruit }}
+            </li>
+        </ul>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'HelloWorld',
+    data() {
+        return {
+            fruits: [
+                'Apple', 'Banana', 'Cherry'
+            ]
+        }
+    },
+    computed: {
+        hasFruit() {
+            return this.fruits.length > 0
+        },
+        reverseFruits() {
+            return this.fruits.map(fruit => {
+                return fruit.split('').reverse().join('')
+            })
+        }
+    }
+}
+</script>
+
+<style scoped>
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+
+li {
+    margin: 0;
+}
+</style>
