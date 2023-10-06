720363657a3cfa453ed2f5ccb4c7f6956e1d9806
twg0 <fruitsjh@gmail.com>
2023-10-06 15:45:46 +0900

    Computed Getter,Setter - 하나의 패턴으로 사용됨

--- vue3-computed/src/App.vue
+++ vue3-computed/src/App.vue
@@ -1,4 +1,7 @@
 <template>
+    <button @click="add">
+        ADD
+    </button>
     <h1>{{ reversedMessage }}</h1>
     <h1>{{ reversedMessage }}</h1>
     <h1>{{ reversedMessage }}</h1>
@@ -9,25 +12,41 @@
 export default {
     data() {
         return {
+            // Getter, Setter
             msg: 'Hello Computed!'
         }
     },
     computed: {
         // computed 에서 이미 계산된 요소는 재사용될 때 캐싱되어 사용됨
-        reversedMessage() {
-            return this.msg.split('').reverse().join('')
+        // 기본적으로 값을 얻어내는 방식으로만 사용됨 = Getter
+        // reversedMessage() {
+        //     return this.msg.split('').reverse().join('')
+        // }
+        // 예외로 Setter 를 지정해줄 수 있음
+        reversedMessage: {
+            get() {
+                return this.msg.split('').reverse().join('')
+            },
+            set(newValue) {
+                // this.msg = newValue
+                console.log(newValue)
+            }
         }
     },
-    // methods: {
-    //     reverseMessage() {
-    //         return this.msg.split('').reverse().join('')
-    //     }
-    // }
+    methods: {
+        add() {
+            // 여기서 newValue로 !?가 붙은 값이 들어감
+            this.reversedMessage += '!?'
+        }
+    }
 }
 </script>
 
 <style>
 h1,h2,h2,h3,h4,h5,h6 {
+    margin: 0;
+}
+button {
     margin: 0;
 }
 </style>
