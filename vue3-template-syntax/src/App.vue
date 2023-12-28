ee038cfe7c3adb9c260fcf172215708f5c80d732
twg0 <fruitsjh@gmail.com>
2023-12-28 14:17:29 +0900

    설명 추가

--- vue3-template-syntax/src/App.vue
+++ vue3-template-syntax/src/App.vue
@@ -1,7 +1,8 @@
 <template>
 <!--
+스타일 클래스 명인 active를 속성 바인드에 넣는다.
 active 를 '' 으로 감싸는 이유 : 현재 v-bind의 약어로 쓰였기 때문에 단순한 active 는 인식하지 못한다. html과 별개
-msg 를 직접 사용하지 않은 이유 : 클릭시 data의 msg 가 직접 변하기 때문에 h1의 class 가 클릭마다 계속 변하게 되어 style 이 적용되지 않게 된다.-->
+msg 를 속성 바인드 부분에 직접 사용하지 않은 이유 : 클릭시 data의 msg 가 직접 변하기 때문에 h1의 class 가 클릭마다 계속 변하게 되어 style 이 적용되지 않게 된다.-->
     <h1 :[attr]="'active'" @[event]="add">
         {{ msg }}
     </h1>
