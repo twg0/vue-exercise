271a6eb288c00a1a26acdb1bdfb9148a79eed6c5
twg0 <fruitsjh@gmail.com>
2023-12-29 21:33:36 +0900

    slot

---
+++ vue3-slot/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <MyBtn>
+        <!--    slot에 직접 위치를 지정, 그래서 순서가 바뀌어도 괜찮음    -->
+        <!--    v-slot: 은 #으로 약어 사용 가능    -->
+        <template #text>
+            <span>Banana</span>
+        </template>
+        <template #icon>
+            <span>(B)</span>
+        </template>
+        <!--    여기에 있는 모든 내용이 slot을 대체하여 들어가게 됨    -->
+        <!--        <span>Banana</span>-->
+        <!--        <span>(B)</span>-->
+    </MyBtn>
+</template>
+
+<script>
+
+import MyBtn from "@/components/MyBtn.vue";
+
+export default {
+    components: {MyBtn}
+}
+</script>
---
+++ vue3-slot/src/components/MyBtn.vue
@@ -0,0 +1,25 @@
+<template>
+<!--  기본적인 컨텐트를 삽입하면 전부 대체 되는데, 만약 없다면 slot 태그 안에 있는 요소들이 대체된다. slot 사이에 있는 컨텐츠를 fallback(대체) 컨텐츠라고 한다  -->
+    <div
+        class="btn">
+        <slot name="icon"></slot>
+        <slot name="text"></slot>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+.btn {
+    display: inline-block;
+    margin: 4px;
+    padding: 6px 12px;
+    border-radius: 4px;
+    background-color: gray;
+    color: white;
+    cursor: pointer;
+}
+</style>
