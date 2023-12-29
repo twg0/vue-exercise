254fdbc6ca2627a1b347905496faf5a8eaf5a84b
twg0 <fruitsjh@gmail.com>
2023-12-29 13:37:42 +0900

    key-modifier

---
+++ vue3-key-modifier/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+<!--  .enter 엔터가 눌렸을 때만 handler 동작  -->
+<!--  .원하는 키 를 입력하면 handler 동작  -->
+<!--  체이닝으로 작성하면 전부 눌려있어야 동작  -->
+    <input type="text"
+    @keydown.ctrl.a="handler">
+</template>
+
+<script>
+
+export default {
+    methods: {
+        handler() {
+            console.log('Enter!!')
+/*            if (event.key === 'Enter') {
+                console.log('Enter!!');
+            }*/
+        }
+    }
+}
+</script>
+
