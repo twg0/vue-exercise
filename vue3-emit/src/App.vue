127d0a3208042eb0f626cf2b843cc56944ad7825
twg0 <fruitsjh@gmail.com>
2023-12-29 21:21:34 +0900

    emit

---
+++ vue3-emit/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+<!--  이벤트는 요소에 명확히 연결되었을 때 동작함, 이것도 최상위에 속성이 상속되는 것이다  -->
+    <MyBtn @heropy="log" @change-msg="logMsg">
+        Banana
+    </MyBtn>
+</template>
+
+<script>
+
+import MyBtn from "@/components/MyBtn.vue";
+
+export default {
+    components: {MyBtn},
+    methods: {
+        log(event) {
+            console.log('Click!!')
+            console.log(event)
+        },
+        logMsg(msg) {
+            console.log(msg)
+        }
+    }
+}
+</script>
---
+++ vue3-emit/src/components/MyBtn.vue
@@ -0,0 +1,43 @@
+<template>
+    <div
+        class="btn">
+        <slot></slot>
+    </div>
+    <!--  emits에 상위의 click이 전달되고, emit()을 통해 사용이 된다.  -->
+    <!--  상위의 요소가 반드시 click이라는 요소가 아니어도 된다. 이곳에서 click을 하면 emit을 통해 상위의 log 메소드가 동작한다.  -->
+    <!--  dblclick : 더블클릭  -->
+    <!--  두번째 요소로 변수를 줄 수 있음 -->
+    <h1 @dblclick="$emit('heropy', $event)">ABC</h1>
+    <input type="text" v-model="msg"/>
+</template>
+
+<script>
+export default {
+    emits: [
+        'heropy',
+        'changeMsg'
+    ],
+    data() {
+        return {
+            msg: ''
+        }
+    },
+    watch: {
+        msg() {
+            this.$emit('changeMsg', this.msg)
+        }
+    }
+}
+</script>
+
+<style scoped>
+.btn {
+    display: inline-block;
+    margin: 4px;
+    padding: 6px 12px;
+    border-radius: 4px;
+    background-color: gray;
+    color: white;
+    cursor: pointer;
+}
+</style>
